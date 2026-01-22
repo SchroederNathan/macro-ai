6d26fe73b24eea36c4f8d076e5cfd7389a3774a3
“Nathan <schroedernathan2011@icloud.com>
2026-01-22 00:58:07 -0500

    Added experimental right side headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
 import { Stack } from 'expo-router'
-import { View, Appearance } from 'react-native'
-import { KeyboardProvider } from 'react-native-keyboard-controller'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect, useState } from 'react'
-import '../globals.css'
+import { Appearance, View } from 'react-native'
+import { KeyboardProvider } from 'react-native-keyboard-controller'
 import { SafeAreaListener } from 'react-native-safe-area-context'
 import { Uniwind } from 'uniwind'
+import '../globals.css'
 
-import '@/polyfills';
+import '@/polyfills'
+import { GlassView } from 'expo-glass-effect'
+import { EllipsisVertical } from 'lucide-react-native'
+import { isGlassEffectAPIAvailable } from 'expo-glass-effect'
 
 export default function Layout() {
   const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme())
+
+  const isGlassEffectAvailable = isGlassEffectAPIAvailable()
+
 
   useEffect(() => {
     const subscription = Appearance.addChangeListener(({ colorScheme }) => {
@@ -28,8 +34,40 @@ export default function Layout() {
       <View className={`flex-1 ${colorScheme === 'dark' ? 'dark' : 'light'}`}>
         <StatusBar style="auto" />
         <KeyboardProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
+          <Stack>
+            <Stack.Screen name="index" options={{
+              headerTransparent: true,
+              headerLargeStyle: { backgroundColor: 'transparent' },
+              title: '',
+              headerBlurEffect: undefined,
+              unstable_headerRightItems: () => [
+                {
+                  type: 'menu',
+                  label: 'Options',
+                  icon: {
+                    type: 'sfSymbol',
+                    name: 'ellipsis',
+                  },
+                  menu: {
+                    title: 'Options',
+                    items: [
+                      {
+                        type: 'action',
+                        label: 'Edit',
+                        icon: {
+                          type: 'sfSymbol',
+                          name: 'pencil',
+                        },
+                        onPress: () => {
+                          // Do something
+                        },
+                      },
+
+                    ],
+                  },
+                },
+              ],
+            }} />
           </Stack>
         </KeyboardProvider>
       </View>
